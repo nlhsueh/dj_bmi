b632a021d97b5b8265c70153cd216f40fb0ea800
nien lin hsueh <nlhsueh@gmail.com>
2023-05-29 09:20:21 +0800

    fix the bug in form; add extraCS

--- bmi/urls.py
+++ bmi/urls.py
@@ -6,4 +6,5 @@ from . import views
 urlpatterns = [
     path('bmi/', views.home, name='home'),
     path('bmi/details/<int:id>', views.detail, name='detail'),
+    path('bmi/update/<int:id>', views.update, name='update'),
 ]
--- bmi/views.py
+++ bmi/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from bmi.models import People, Level
+from .forms import PeopleModelForm
 
-# Create your views here.
-
+def setBMI(p):
+    p.bmi = round(p.w/(p.h/100)**2, 2)
 
 def home(request):
     print('request: ', request)
@@ -11,14 +12,14 @@ def home(request):
     print('pList: ', pList)
 
     for p in pList:
-        p.bmi = round(p.w/(p.h/100)**2, 2)
+        # p.bmi = round(p.w/(p.h/100)**2, 2)
+        setBMI(p)
         print('After adding bmi to p: ', p, p.bmi)
 
     param = {
         'plist': pList
     }
     return render(request, 'peoples.html', context=param)
-
 
 def detail(request, id):
     p = People.objects.get(id=id)
@@ -29,3 +30,27 @@ def detail(request, id):
         'p': p
     }
     return render(request, 'details.html', context=param)
+
+def update(request, id):
+    # POST
+    if request.method == "POST":
+        p = People.objects.get(id = id)
+        if p: 
+            form = PeopleModelForm(request.POST, instance = p)
+            if form.is_valid():
+                form.save()
+                print ('form saved')
+                setBMI(p)
+                return render(request, "details.html", {'p': p}) 
+            else: 
+                return render(request, "find_fail.html", {'error_msg': str(form.errors)}) 
+        else:
+            print ('form.errors: ', form.errors)
+            return render(request, "find_fail.html", {'error_msg': 'ID not found'}) 
+    
+    # GET
+    p = People.objects.get(id=id)
+    if p: 
+        return render(request, "update_people.html", {'p': p})
+    else:
+        return render(request, "find_fail.html", {'error_msg': 'ID not found'}) 
